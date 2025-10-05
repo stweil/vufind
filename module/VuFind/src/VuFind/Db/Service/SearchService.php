9c5128f74afe06adcb9bf67c91fbd641342441c4
Stefan Weil <sw@weilnetz.de>
2025-10-05 19:34:21 +0200

    Replace SearchEntityInterface::class
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- module/VuFind/src/VuFind/Db/Service/SearchService.php
+++ module/VuFind/src/VuFind/Db/Service/SearchService.php
@@ -54,7 +54,7 @@ class SearchService extends AbstractDbService implements
      */
     public function createEntity(): SearchEntityInterface
     {
-        return $this->entityPluginManager->get(SearchEntityInterface::class);
+        return $this->entityPluginManager->get(\VuFind\Db\Entity\Search::class);
     }
 
     /**
@@ -98,7 +98,7 @@ class SearchService extends AbstractDbService implements
     {
         $userId = $userOrId instanceof UserEntityInterface ? $userOrId->getId() : $userOrId;
         $parameters = ['saved' => false, 'sessionId' => $sessionId];
-        $dql = 'DELETE FROM ' . SearchEntityInterface::class . ' s '
+        $dql = 'DELETE FROM ' . \VuFind\Db\Entity\Search::class . ' s '
             . 'WHERE s.saved = :saved AND (s.sessionId = :sessionId';
         if ($userId !== null) {
             $dql .= ' OR s.user = :userId';
@@ -119,7 +119,7 @@ class SearchService extends AbstractDbService implements
      */
     public function getSearchById(int $id): ?SearchEntityInterface
     {
-        return $this->entityManager->find(SearchEntityInterface::class, $id);
+        return $this->entityManager->find(\VuFind\Db\Entity\Search::class, $id);
     }
 
     /**
@@ -137,7 +137,7 @@ class SearchService extends AbstractDbService implements
         UserEntityInterface|int|null $userOrId
     ): ?SearchEntityInterface {
         $userId = $userOrId instanceof UserEntityInterface ? $userOrId->getId() : $userOrId;
-        $entityClass = SearchEntityInterface::class;
+        $entityClass = \VuFind\Db\Entity\Search::class;
 
         $dql = 'SELECT s FROM ' . $entityClass . ' s WHERE s.id = :id AND (s.sessionId = :sessionId';
         $parameters = [
@@ -174,7 +174,7 @@ class SearchService extends AbstractDbService implements
             return [];
         }
 
-        $entityClass = SearchEntityInterface::class;
+        $entityClass = \VuFind\Db\Entity\Search::class;
         $dql = 'SELECT s FROM ' . $entityClass . ' s';
         $conditions = [];
         $params = [];
@@ -209,7 +209,7 @@ class SearchService extends AbstractDbService implements
      */
     public function getScheduledSearches(): array
     {
-        $entityClass = SearchEntityInterface::class;
+        $entityClass = \VuFind\Db\Entity\Search::class;
         $dql = 'SELECT s FROM ' . $entityClass
             . ' s WHERE s.saved = :saved'
             . ' AND s.notificationFrequency > 0'
@@ -237,7 +237,7 @@ class SearchService extends AbstractDbService implements
         UserEntityInterface|int|null $userOrId = null
     ): array {
         $userId = $userOrId instanceof UserEntityInterface ? $userOrId->getId() : $userOrId;
-        $dql = 'SELECT s FROM ' . SearchEntityInterface::class . ' s '
+        $dql = 'SELECT s FROM ' . \VuFind\Db\Entity\Search::class . ' s '
             . 'WHERE s.checksum = :checksum AND ';
         $extraClauses = ['(s.sessionId = :sessionId AND s.saved = :saved)'];
         $params = ['checksum' => $checksum, 'saved' => false, 'sessionId' => $sessionId];
@@ -259,7 +259,7 @@ class SearchService extends AbstractDbService implements
      */
     public function getSavedSearchesWithMissingChecksums(): array
     {
-        $dql = 'SELECT s FROM ' . SearchEntityInterface::class . ' s '
+        $dql = 'SELECT s FROM ' . \VuFind\Db\Entity\Search::class . ' s '
             . 'WHERE s.checksum IS NULL AND s.saved = :saved';
 
         $query = $this->entityManager->createQuery($dql);
@@ -277,7 +277,7 @@ class SearchService extends AbstractDbService implements
     public function deleteSearch(SearchEntityInterface|int $searchOrId): void
     {
         $searchId = $searchOrId instanceof SearchEntityInterface ? $searchOrId->getId() : $searchOrId;
-        $dql = 'DELETE FROM ' . SearchEntityInterface::class . ' s'
+        $dql = 'DELETE FROM ' . \VuFind\Db\Entity\Search::class . ' s'
             . ' WHERE s.id = :searchId';
         $query = $this->entityManager->createQuery($dql);
         $query->setParameter('searchId', $searchId);
@@ -296,7 +296,7 @@ class SearchService extends AbstractDbService implements
     {
         $subQueryBuilder = $this->entityManager->createQueryBuilder();
         $subQueryBuilder->select('s.id')
-            ->from(SearchEntityInterface::class, 's')
+            ->from(\VuFind\Db\Entity\Search::class, 's')
             ->where('s.created < :dateLimit AND s.saved = :saved')
             ->setParameter('dateLimit', $dateLimit)
             ->setParameter('saved', false);
@@ -305,7 +305,7 @@ class SearchService extends AbstractDbService implements
             $subQueryBuilder->setMaxResults($limit);
         }
         $queryBuilder = $this->entityManager->createQueryBuilder();
-        $queryBuilder->delete(SearchEntityInterface::class, 's')
+        $queryBuilder->delete(\VuFind\Db\Entity\Search::class, 's')
             ->where('s.id IN (:searches)')
             ->setParameter('searches', $subQueryBuilder->getQuery()->getResult());
 

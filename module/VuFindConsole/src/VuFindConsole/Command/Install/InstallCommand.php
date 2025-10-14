abcf6784e42070d053825cedbc86658ef46381a0
Stefan Weil <sw@weilnetz.de>
2025-10-14 09:46:07 +0200

    Fix help text for install.php
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- module/VuFindConsole/src/VuFindConsole/Command/Install/InstallCommand.php
+++ module/VuFindConsole/src/VuFindConsole/Command/Install/InstallCommand.php
@@ -169,7 +169,7 @@ class InstallCommand extends Command
                 null,
                 InputOption::VALUE_REQUIRED,
                 'What base path should be used in VuFind®\'s URL?'
-                . " (defaults to {$this->baseDir} when --non-interactive is set)"
+                . " (defaults to {$this->basePath} when --non-interactive is set)"
             )->addOption(
                 'multisite',
                 null,
